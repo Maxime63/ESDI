4a62a033cb4bc540ba2295ebc2ee5af9931b460a
Maxime63 <maxime.chauvet@bbox.fr>
2014-12-21 19:50:22 +0100

    Debut fenêtre

---
+++ src/com/tp/edsi/vue/Fenetre.java
@@ -0,0 +1,12 @@
+package com.tp.edsi.vue;
+
+import javax.swing.JFrame;
+
+public class Fenetre extends JFrame{
+
+	/***/
+	private static final long serialVersionUID = 1L;
+
+	public Fenetre() {
+	}
+}
